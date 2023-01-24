8615d899bb4735977fa83292baf4e8c1d6fb08e1
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-01-24 10:53:51 +0530

    Update settings.py

--- skillversity/settings.py
+++ skillversity/settings.py
@@ -132,7 +132,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
