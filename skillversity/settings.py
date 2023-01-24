708a3b4d2f56d7276633bb689ed0d20a13559fee
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-01-24 10:58:04 +0530

    Update settings.py

--- skillversity/settings.py
+++ skillversity/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/home/ubuntu/skillversity.com/static/'
 
 STORAGES = {
     "staticfiles": {
