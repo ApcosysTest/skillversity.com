ac71303af61bd369c1f0e77c423d9322140ca350
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-01-27 13:58:26 +0530

    Update storage_backends.py

--- skillversity/storage_backends.py
+++ skillversity/storage_backends.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from storage.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
-  location = settings.MEDIAFILES_LOCATION
+  location = 'media'
   file_overwrite = False
