ece4b6fdcb3ae8b4746049e571254363ba108618
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-01-27 13:58:44 +0530

    Update settings.py

--- skillversity/settings.py
+++ skillversity/settings.py
@@ -151,7 +151,8 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-MEDIAFILES_LOCATION = 'media'
+MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'skillversity.storage_backends.MediaStorage'
 
 # Send Email 
