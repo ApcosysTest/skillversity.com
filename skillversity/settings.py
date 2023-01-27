31d7d7a1ca49395eb2130ff2ba8994fc9b8d1154
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-01-27 13:42:21 +0530

    Update settings.py

--- skillversity/settings.py
+++ skillversity/settings.py
@@ -144,9 +144,15 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'skillversity.storage_backends.MediaStorage'
 
 # Send Email 
 EMAIL_BACKEND = str(os.getenv('EMAIL_BACKEND')) 
