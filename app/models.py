142e54d802b63ddb4368f20327e0782e9828ed12
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-02-09 12:13:37 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -31,6 +31,7 @@ class Investor(models.Model):
     name = models.CharField(max_length=20)  
     email = models.EmailField()  
     contact = models.CharField(max_length=10)
+    textarea = models.CharField(max_length=500)
         
     def __str__(self):
         return self.name
