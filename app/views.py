e17e1abadf83c59cf70ff874407d617a3ab2262d
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-02-09 17:58:53 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, logout, login as auth_login, updat
 from .helper import send_forget_password_mail
 import uuid
 from django.contrib.auth.decorators import login_required, user_passes_test
-import json, urllib
+import json, urllib.request
 from django.conf import settings
 
 # Create your views here.
