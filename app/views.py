987909024e762c4f12561cceb0527c8313ab30e7
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-02-14 16:31:57 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -259,9 +259,12 @@ def courseDetail(request, id):
     silver_sum, gold_sum, gold_total, bundle_sp,bundle_gp = 0, 0, 0, 0, 0
     bundle = Bundle.objects.all()
     bundle_head = Bundle.objects.get(id=id)
+    dollar = Currency.objects.get(currency="US Dollar")
     silver = Course.objects.filter(category__name='Silver',bundle_id=id).order_by('name')
     for i in silver:
         silver_sum = silver_sum + i.price
+        dollprice = round(i.price / dollar.equivalent_rupee, 2)
+        dolls.append({'id':i.id, 'value':dollprice})
     gold = Course.objects.filter(category__name='Gold',bundle_id=id).order_by('name')
     for i in gold:
         gold_sum = gold_sum + i.price
@@ -271,11 +274,9 @@ def courseDetail(request, id):
         pass
     else:
         flag = True
-        dollar = Currency.objects.get(currency="US Dollar")
-        bundle_head = Bundle.objects.get(id=id)
         bundle_sp = round(bundle_head.silver_price / dollar.equivalent_rupee, 2)
         bundle_gp = round(bundle_head.gold_price / dollar.equivalent_rupee, 2)
-    context = {'bundle':bundle, 'bundle_head':bundle_head, 'silver':silver, 'gold':gold, 'silver_sum':silver_sum, 'gold_sum':gold_sum,'gold_total':gold_total,'bundle_sp':bundle_sp,'bundle_gp':bundle_gp, 'flag':flag}
+    context = {'bundle':bundle, 'bundle_head':bundle_head, 'silver':silver, 'gold':gold, 'silver_sum':silver_sum, 'gold_sum':gold_sum,'gold_total':gold_total,'bundle_sp':bundle_sp,'bundle_gp':bundle_gp, 'flag':flag, 'dolls':dolls}
     return render(request, 'courseDetail.html', context)
 
 def get_location(request):
